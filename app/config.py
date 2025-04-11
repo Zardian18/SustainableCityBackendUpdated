d0198a1572e617c1f74cb2a847c7c47ed28248c1
Uday <udaynl619@gmail.com>
2025-04-11 11:55:45 +0100

    modified firestore json path

--- app/config.py
+++ app/config.py
@@ -5,7 +5,7 @@ load_dotenv()
 
 
 # Firestore Configuration
-FIRESTORE_CREDENTIALS = os.getenv('FIRESTORE_CREDENTIALS', 'C:\\Users\\udaym\\Documents\\Assignments\\ASE\\SustainableCityBackendUpdated\\serviceAccount.json')
+FIRESTORE_CREDENTIALS = os.getenv('FIRESTORE_CREDENTIALS', 'serviceAccount.json')
 
 # Flask Configuration
 SECRET_KEY = os.getenv('SECRET_KEY', 'jwtsecretkey12312323CASDdd23dddDFASF')
